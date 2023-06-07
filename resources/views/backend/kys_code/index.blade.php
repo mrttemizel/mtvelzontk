081e52cd15fe609ab8cdcc9de6ddabb8a9cab6d3
mt <murat.temizel@antalya.eduy>
2023-06-07 21:34:15 +0300

    datatable added

--- resources/views/backend/kys_code/index.blade.php
+++ resources/views/backend/kys_code/index.blade.php
@@ -1,27 +1,65 @@
 @extends('backend.components.master')
 @section('title')
-    Kullanıcı Ekle
+    KYS Kodu Listesi
 @endsection
 @section('css')
-    <link href="{{asset('backend/assets/libs/sweetalert2/sweetalert2.min.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('backend/assets/css/bootstrap.min.css')}}' rel="stylesheet" type="text/css" />
 
-    <!--datatable css-->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css"/>
-    <!--datatable responsive css-->
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css"/>
+    <link href="{{asset('backend/assets/libs/sweetalert2/sweetalert2.min.css')}}" rel="stylesheet" type="text/css" />
 
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
-
+    <link rel="stylesheet" href="{{asset('backend/assets/libs/datatable/jquery.dataTables.css')}}" rel="stylesheet" />
 @endsection
 @section('content')
     @component('backend.components.breadcrumb')
         @slot('li_1')
-            Kullanıcılar
+            KYS
         @endslot
         @slot('title')
-            Kullanıcı Listesi
+            KYS Kodu Listesi
         @endslot
     @endcomponent
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h5 class="card-title mb-0">KYS Kodu Listesi</h5>
+                    <button type="button" class="btn btn-primary waves-effect waves-light d-flex justify-content-between"><i class="ri-add-box-line"></i> KYS Kodu Ekle</button>
+                </div>
+                <div class="card-body">
+                    <table id="datatable" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                        <tr>
+                            <th>SR No.</th>
+                            <th>KYS Kodu</th>
+                            <th>KYS Açıklama</th>
+                            <th>KYS Kategori</th>
+                            <th>Düzenle</th>
+                        </tr>
+                        </thead>
+                        <tbody
+                        @php $i = 0 @endphp
+                        @foreach($data as $datas)
+                            @php $i++ @endphp
+                            <tr>
+                                <td>{{$i}}</td>
+                                <td>{{$datas->code_name}}</td>
+                                <td>{{$datas->code_description}}</td>
+                                <td>{{$datas->getKysCategory->kys_category_name}}</td>
+                                <td>
+                                    <div class="hstack gap-3 fs-15">
+                                        <a href="" class="link-primary"><i class="ri-settings-4-line"></i></a>
+                                        <a href="" class="link-danger" id="delete_user"><i class="ri-delete-bin-5-line"></i></a>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+
+                            </tbody>
+                    </table>
+                </div>
+            </div>
+        </div><!--end col-->
+    </div><!--end row-->
 
 
 
@@ -29,14 +67,21 @@
 @endsection
 
 @section('addjs')
+
+        <script src="{{ asset('backend/assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/js/pages/sweetalerts.init.js') }}"></script>
+
     <!--datatable js-->
-    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+        <script src="{{asset("backend/assets/libs/datatable/jquery.dataTables.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/dataTables.bootstrap5.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/dataTables.buttons.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/buttons.bootstrap5.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/jszip.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/pdfmake.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/vfs_fonts.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/buttons.html5.min.js")}}"></script>
 
-
-    <script src="{{asset('backend/assets/js/pages/datatables.init.js')}}"></script>
+        <script src="{{asset('backend/assets/libs/datatable/mydatatable.js')}}"></script>
 
 @endsection
 
--- resources/views/backend/user/index.blade.php
+++ resources/views/backend/user/index.blade.php
@@ -5,12 +5,8 @@
 @section('css')
     <link href="{{asset('backend/assets/libs/sweetalert2/sweetalert2.min.css')}}" rel="stylesheet" type="text/css" />
 
-    <!--datatable css-->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" />
-    <!--datatable responsive css-->
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
 
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
 
 @endsection
 @section('content')
@@ -48,7 +44,7 @@
                                 <h5 class="card-title mb-0">Kullanıcı Listesi</h5>
                             </div>
                             <div class="card-body">
-                                <table id="alternative-pagination" class="table nowrap dt-responsive align-middle table-hover table-bordered" style="width:100%">
+                                <table id="datatable" class="display" style="width:100%">
                                     <thead>
                                     <tr>
                                         <th>SR No.</th>
@@ -83,9 +79,9 @@
                                                     <a href="javascript:void(0)" data-url={{route('users.delete', ['id'=>$datas->id]) }} data-id={{ $datas->id }} class="link-danger" id="delete_user"><i class="ri-delete-bin-5-line"></i></a>                                                </div>
                                             </td>
                                         </tr>
-                                    @endforeach
+                                        @endforeach
 
-                                    </tbody>
+                                        </tbody>
                                 </table>
                             </div>
                         </div>
@@ -103,34 +99,16 @@
     <script src="{{ asset('backend/assets/js/pages/sweetalerts.init.js') }}"></script>
 
     <!--datatable js-->
-    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="{{asset("backend/assets/libs/datatable/jquery.dataTables.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/dataTables.bootstrap5.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/dataTables.buttons.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/buttons.bootstrap5.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/jszip.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/pdfmake.min.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/vfs_fonts.js")}}"></script>
+    <script src="{{asset("backend/assets/libs/datatable/buttons.html5.min.js")}}"></script>
 
-
-    <script src="{{asset('backend/assets/js/pages/datatables.init.js')}}"></script>
-
-    <script>
-        $(document).on('click', '#delete_user', function () {
-            var user_id = $(this).attr('data-id');
-            const url = $(this).attr('data-url');
-            Swal.fire({
-                title: 'Emin misiniz?',
-                text: "Bu kullanıcıyı silmek istediğinize emin misiniz?",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Evet, sil!',
-                cancelButtonText: 'Vazgeç'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = url;
-                }
-            });
-        });
-    </script>
+    <script src="{{asset('backend/assets/libs/datatable/mydatatable.js')}}"></script>
 
 @endsection
 
